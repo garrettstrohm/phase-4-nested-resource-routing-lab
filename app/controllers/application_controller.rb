cdb073734e57b54aeb9efce725b41c3c415f2bda
Garrett <garres4@vt.edu>
2021-12-19 14:50:09 -0700

    commit tree 7a51570018d6b31f23d634cf2dfec340a49135f1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+    rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_record_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
+  def rescue_from_record_not_found exception_obj
+    render json: {error: "#{exception_obj.model} not found"}, status: :not_found
+  end
+
+  def invalid_record(exception_obj)
+    render json: {error: exception_obj.record.errors.full_messages }, status: :unprocessable_entity
+  end
+  
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = find_user
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+      user = find_user
+      item = user.items.find(params[:id])
+      render json: item, include: :user
+  end
+
+  def create
+    user = find_user
+    item = user.items.create!(item_params)
+    render json: item, status: :created
+  end
+
+  private 
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def item_params
+    params.permit(:id, :name, :description, :price, :user_id)
   end
 
 end
